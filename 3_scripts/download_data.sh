f9996a11ef8d87934e91cdf7722eeec99f9f5d7b
Rianne Passos Lopes <rplopes@proton.me>
2026-06-27 20:34:36 +0000

    Corrige o script download_data.sh

--- 3_scripts/download_data.sh
+++ 3_scripts/download_data.sh
@@ -1,6 +1,6 @@
 # Configurar dir
 DR_DIR="../1_data/1_raw/"
 # Baixar dados
-fasterq-dump -o ${DR_DIR}DRR030428 --progress
+fasterq-dump DRR030428 -o ${DR_DIR} --progress
 # Compactar dados para o QIIME2
 pigz ${DR_DIR}DRR030428_1.fastq ${DR_DIR}DRR030428_2.fastq

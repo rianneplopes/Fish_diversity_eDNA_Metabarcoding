f50bb0a89bc4fcfc1b17023e7a8629d637e2025b
Rianne Passos Lopes <rplopes@proton.me>
2026-06-28 00:21:29 +0000

    Script para gerar manifest.tsv correto

---
+++ 3_scripts/generate_manifest_file.sh
@@ -0,0 +1,11 @@
+cd "../1_data/"
+printf "sample-id\tforward-absolute-filepath\treverse-absolute-filepath\n" > ../1_data/manifest.tsv
+printf "DRR030421\t/workspaces/Fish_diversity_eDNA_Metabarcoding/1_data/1_raw/DRR030421_1.fastq.gz\t/workspaces/Fish_diversity_eDNA_Metabarcoding/1_data/1_raw/DRR030421_2.fastq.gz\n" >> ../1_data/manifest.tsv
+printf "DRR030422\t/workspaces/Fish_diversity_eDNA_Metabarcoding/1_data/1_raw/DRR030422_1.fastq.gz\t/workspaces/Fish_diversity_eDNA_Metabarcoding/1_data/1_raw/DRR030422_2.fastq.gz\n" >> ../1_data/manifest.tsv
+printf "DRR030423\t/workspaces/Fish_diversity_eDNA_Metabarcoding/1_data/1_raw/DRR030423_1.fastq.gz\t/workspaces/Fish_diversity_eDNA_Metabarcoding/1_data/1_raw/DRR030423_2.fastq.gz\n" >> ../1_data/manifest.tsv
+printf "DRR030424\t/workspaces/Fish_diversity_eDNA_Metabarcoding/1_data/1_raw/DRR030424_1.fastq.gz\t/workspaces/Fish_diversity_eDNA_Metabarcoding/1_data/1_raw/DRR030424_2.fastq.gz\n" >> ../1_data/manifest.tsv
+printf "DRR030425\t/workspaces/Fish_diversity_eDNA_Metabarcoding/1_data/1_raw/DRR030425_1.fastq.gz\t/workspaces/Fish_diversity_eDNA_Metabarcoding/1_data/1_raw/DRR030425_2.fastq.gz\n" >> ../1_data/manifest.tsv
+printf "DRR030426\t/workspaces/Fish_diversity_eDNA_Metabarcoding/1_data/1_raw/DRR030426_1.fastq.gz\t/workspaces/Fish_diversity_eDNA_Metabarcoding/1_data/1_raw/DRR030426_2.fastq.gz\n" >> ../1_data/manifest.tsv
+printf "DRR030427\t/workspaces/Fish_diversity_eDNA_Metabarcoding/1_data/1_raw/DRR030427_1.fastq.gz\t/workspaces/Fish_diversity_eDNA_Metabarcoding/1_data/1_raw/DRR030427_2.fastq.gz\n" >> ../1_data/manifest.tsv
+printf "DRR030428\t/workspaces/Fish_diversity_eDNA_Metabarcoding/1_data/1_raw/DRR030428_1.fastq.gz\t/workspaces/Fish_diversity_eDNA_Metabarcoding/1_data/1_raw/DRR030428_2.fastq.gz\n" >> ../1_data/manifest.tsv
+cd "../3_scripts/"

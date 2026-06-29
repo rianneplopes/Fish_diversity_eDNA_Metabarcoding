723362aa47843dcc8107ed08c9680d868dfd7f16
Rianne Passos Lopes <rplopes@proton.me>
2026-06-29 14:41:32 -0300

    Run dada2

--- 3_scripts/7_view_dada2.sh
+++ 3_scripts/7_view_dada2.sh
@@ -1,15 +1,17 @@
+export TZ="UTC"
+
 # Estatística do denoising
 qiime metadata tabulate \
-  --m-input-file ../4_output/denoising-stats.qza \
-  --o-visualization ../4_output/denoising-stats.qzv
+  --m-input-file ../4_output/denoising_stats.qza \
+  --o-visualization ../4_output/denoising_stats.qzv
 
 # Resumo da tabela de features (ASVs)
 qiime feature-table summarize \
   --i-table    ../4_output/table.qza \
-  --m-sample-metadata-file ../1_data/sample-metadata.tsv \
+  --m-sample-metadata-file ../2_metadata/sample_metadata.tsv \
   --o-visualization ../4_output/table.qzv
 
 # Tabela das sequências das ASVs
 qiime feature-table tabulate-seqs \
-  --i-data    ../4_output/rep-seqs.qza \
-  --o-visualization ../4_output/rep-seqs.qzv
+  --i-data    ../4_output/rep_seqs.qza \
+  --o-visualization ../4_output/rep_seqs.qzv

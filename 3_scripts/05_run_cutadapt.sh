925da7d19b17509672c0f94b432dc982936ae48a
Rianne Passos Lopes <rplopes@proton.me>
2026-07-05 19:22:23 +0000

    Rodando análises de diversidade

--- 3_scripts/05_run_cutadapt.sh
+++ 3_scripts/05_run_cutadapt.sh
@@ -7,7 +7,7 @@ qiime cutadapt trim-paired \
   --p-cores 4 \
   --p-match-read-wildcards \
   --o-trimmed-sequences ../4_output/1_qiime_artifacts/demux_trimmed.qza \
-  --verbose > ../4_output/3_logs/cutadapt_report.txt 2>&1
+  --verbose > ../4_output/3_logs/cutadapt.log 2>&1
 
 
   # Primers MiFish-F e MiFish-R disponíveis em 10.1098/rsos.150088
--- 3_scripts/13_diversity.sh
+++ 3_scripts/13_diversity.sh
@@ -1,18 +1,69 @@
-
-OP_DIR="../4_output/1_qiime_artifacts"
-
 export TZ="UTC"
-qiime phylogeny align-to-tree-mafft-fasttree \
-  --i-sequences ${OP_DIR}/rep_seqs.qza \
-  --o-alignment ${OP_DIR}/aligned_rep_seqs.qza \
-  --o-masked-alignment ${OP_DIR}/masked_aligned_rep_seqs.qza \
-  --o-tree ${OP_DIR}/unrooted_tree.qza \
-  --o-rooted-tree ${OP_DIR}/rooted_tree.qza \
-  --p-n-threads 4
 
+mkdir ../4_output/4_core_metrics/
+
+ARTIFACTS_DIR="../4_output/1_qiime_artifacts"
+METADATA_DIR="../2_metadata"
+CORE_METRICS_DIR="../4_output/4_core_metrics"
+LOG_DIR="../4_output/3_logs"
+
+TABLE_QZA="${ARTIFACTS_DIR}/table.qza"
+METADATA_TSV="${METADATA_DIR}/sample_metadata.tsv"
+
+echo "Gerando a curva de rarefação..."
+
+# Curva de rarefação alfa (para avaliar o esforço amostral)
 qiime diversity alpha-rarefaction \
-  --i-table ${OP_DIR}/table.qza \
-  --i-phylogeny ${OP_DIR}/rooted_tree.qza \
-  --p-max-depth 500 \
-  --m-metadata-file ..2/metadata/sample_metadata.tsv \
-  --o-visualization ${OP_DIR}/alpha_rarefaction.qzv
+  --i-table ${TABLE_QZA} \
+  --p-max-depth 100000 \
+  --m-metadata-file ${METADATA_TSV} \
+  --o-visualization ${ARTIFACTS_DIR}/alpha_rarefaction_report.qzv
+
+echo "Calculando as matrizes de diversidade alfa e beta..."
+
+# Métricas principais de diversidade biológica
+## Alfa: observed_features (riqueza), shannon_vector, simpson_vector, evenness_vector
+## Beta: jaccard_distance_matrix, bray_curtis_distance_matrix, matrizes de pcoa em .qzv
+qiime diversity core-metrics \
+  --i-table $TABLE_QZA \
+  --p-sampling-depth 45000 \
+  --m-metadata-file $METADATA_TSV \
+  --output-dir $CORE_METRICS_DIR
+### O valor de 45000 foi escolhido com base na frequência mínima de reads nas amostras
+
+echo "Executando testes de hipótese para diversidade alfa..."
+
+# Testes estatísticos de diversidade alfa (Kruskal-Wallsi)
+## Riqueza de espécies (observed_features)
+qiime diversity alpha-group-significance \
+  --i-alpha-diversity ${CORE_METRICS_DIR}/observed_features_vector.qza \
+  --m-metadata-file $METADATA_TSV \
+  --o-visualization ${CORE_METRICS_DIR}/observed_features_significance.qzv
+
+# Shannon (diversidade/equitabilidade)
+qiime diversity alpha-group-significance \
+  --i-alpha-diversity ${CORE_METRICS_DIR}/shannon_vector.qza \
+  --m-metadata-file $METADATA_TSV \
+  --o-visualization ${CORE_METRICS_DIR}/shannon_significance.qzv
+
+echo "Executando testes de hipótese para diversidade beta..."
+
+# Testes estatísticos de diversidade beta
+## Jaccard (turnorver de espécies entre as escalas)
+qiime diversity beta-group-significance \
+  --i-distance-matrix ${CORE_METRICS_DIR}/jaccard_distance_matrix.qza \
+  --m-metadata-file ${METADATA_TSV} \
+  --m-metadata-column escala_local \
+  --p-pairwise \
+  --o-visualization ${CORE_METRICS_DIR}/jaccard_significance.qzv
+  
+  ## Bray-Curtis (estrutura de abundância relativa das leituras)
+
+  qiime diversity beta-group-significance \
+  --i-distance-matrix ${CORE_METRICS_DIR}/bray_curtis_distance_matrix.qza \
+  --m-metadata-file ${METADATA_TSV} \
+  --m-metadata-column escala_local \
+  --p-pairwise \
+  --o-visualization ${CORE_METRICS_DIR}/bray_curtis_significance.qzv
+
+echo "Análises concluídas."
